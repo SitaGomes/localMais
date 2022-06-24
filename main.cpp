633a72a6b6c8c7f25e7805da580588e9f23f5435
SitaGomes <ansitagomes@gmail.com>
2022-06-24 21:01:06 +0000

    feat: criar locação [incompleta]

--- main.cpp
+++ main.cpp
@@ -1,56 +1,69 @@
 //INCLUDING BUILT-IN LIBRARIES...
 #include <iostream>
 #include <string>
-#include <stdlib.h>
+#include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
-class Newlocacao {
-    private:
-        int codigoCliente;
-        int codigoVeiculo;
+class Locacao {
+    private: 
+        int codigoDoCliente; 
     public: 
         int codigoLocacao;
         int dataDaRetirada;
         int dataDaDevolucao;
+        int quantidadeDeOcupantes;
         bool seguro;
-        int quantidadeDeDias;
 
         //? Funcção que vai ser chamada de primeira, para registrar todas as variaveis publicas
-        Newlocacao(int codigoLocacao_, int dataDaRetirada_, int dataDaDevolucao_, bool seguro_, int quantidadeDeDias_) {
-            codigoLocacao = codigoLocacao_;
+        Locacao(int codigoDoCliente_, int dataDaRetirada_, int dataDaDevolucao_, int quantidadeDeOcupantes_, bool seguro_) {
+            codigoDoCliente = codigoDoCliente_;
             dataDaRetirada = dataDaRetirada_;
             dataDaDevolucao = dataDaDevolucao_;
+            quantidadeDeOcupantes = quantidadeDeOcupantes_;
             seguro = seguro_;
-            quantidadeDeDias = quantidadeDeDias_;
+
+            srand(time(0));
+            codigoLocacao = rand(); 
         }
 
-        //? Funções para registrar as variaveis privadas, por segurança
-        void registrarCliente(int codigoCliente_) {
-            codigoCliente = codigoCliente_;
-        }
-        void registrarVeiculo(int codigoVeiculo_) {
-            codigoVeiculo = codigoVeiculo_;
+        //? Funcção principal que retorna a quantidade de diárias
+        int criarLocacao() {
+            int horasAtuais;
+            cout << "Ola caro cliente, ";
+            cout << "quantas horas são? [24h]: ";
+            cin >> horasAtuais;
+
+            if (!((horasAtuais > 8) && (horasAtuais < 18))) {
+                cout << "Desculpe o encomodo, porém estamos fechados" << endl;
+                return 0;
+            }
+
+            if (horasAtuais < 12) 
+                cout << "Bom dia!" << endl;
+            else 
+                cout << "Boa tarde!" << endl;
+
+            return 1;
         }
 
         //?
-        void list() {
-            cout << "Codigo do Cliente "<<codigoCliente << endl;
-            cout <<"Codigo do veiculo "<< codigoVeiculo << endl;
-            cout <<"Codigo da locação "<< codigoLocacao << endl;
-            cout <<"Data da devolução "<< dataDaDevolucao << endl;
-            cout <<"Data da retirada "<< dataDaRetirada << endl;
-            cout <<"Quantidade de Dias "<< quantidadeDeDias << endl;
-            cout <<"Seguro "<< seguro << endl;
+        void listar() {
+            cout << "Codigo da locação -> " << codigoLocacao << endl;
+            cout << "Codigo do cliente -> " << codigoDoCliente << endl;
+            cout << "Data da retirada -> "<< dataDaRetirada << endl;
+            cout << "Data da devolução -> "<< dataDaDevolucao << endl;
+            cout << "Quantidade de Ocupantes -> "<< quantidadeDeOcupantes << endl;
+            cout << "Seguro -> "<< seguro << endl;
         }
 };
 
 
 int main () {
-    Newlocacao location(1, 2, 3, true, 5);
-    location.registrarCliente(12233);
-    location.registrarVeiculo(33333);
+    Locacao location(3333, 2, 3, 5, true);
 
-    location.list();
-
+    location.criarLocacao();
 }
+
+
